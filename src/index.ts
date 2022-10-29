c6efb77bd05b62218fe66f1772f308162b8e2688
Alessandro Talmi <alessandro.talmi@studio.unibo.it>
2022-10-29 19:24:22 +0200

    removed fake mac address, used uuid

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
+import { v4 as uuidv4 } from 'uuid';
 import { InterconnectedNodeBuilder } from 'interconnected_node';
 import onIncomingConnectionsHandler from './onIncomingConnectionsHandler';
-const getmac = require('getmac');
 
 const BROKER_SERVICE_ADDRESS =
   'http://ec2-3-208-18-248.compute-1.amazonaws.com:8000';
-const MY_ID = getmac.default();
+const MY_ID = uuidv4();
 const node = new InterconnectedNodeBuilder().build();
 
-console.log('My MAC address: ' + MY_ID);
+console.log('My ID: ' + MY_ID);
 node.start(
   BROKER_SERVICE_ADDRESS,
   MY_ID,
