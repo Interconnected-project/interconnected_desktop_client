1fb408215d83ec26d38cca0e079ca9b39432e728
Alessandro Talmi <alessandro.talmi@studio.unibo.it>
2022-10-29 18:32:08 +0200

    id set using mac address

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import { InterconnectedNodeBuilder } from 'interconnected_node';
 import onIncomingConnectionsHandler from './onIncomingConnectionsHandler';
+const getmac = require('getmac');
 
 const BROKER_SERVICE_ADDRESS =
   'http://ec2-3-208-18-248.compute-1.amazonaws.com:8000';
-const MY_ID = 'TODO GENERATE AN ID';
+const MY_ID = getmac.default();
 const node = new InterconnectedNodeBuilder().build();
 
+console.log('My MAC address: ' + MY_ID);
 node.start(
   BROKER_SERVICE_ADDRESS,
   MY_ID,
