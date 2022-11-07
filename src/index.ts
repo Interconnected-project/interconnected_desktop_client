54f1d65ecb49ce1499855d336871ff906496d3aa
Alessandro Talmi <alessandro.talmi@studio.unibo.it>
2022-11-07 22:14:00 +0100

    upgraded to interconnected_node v0.0.15

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
-import InterconnectedNode from 'interconnected_node';
+import InterconnectedNode, { DeviceType } from 'interconnected_node';
 import { v4 as uuidv4 } from 'uuid';
 import DesktopP2PConnectionBuilders from './desktop-specific_node_implementation/DesktopP2PConnectionBuilders';
 
 const BROKER_SERVICE_ADDRESS =
   'http://ec2-3-208-18-248.compute-1.amazonaws.com:8000';
 export const MY_ID = uuidv4();
-const node = new InterconnectedNode(MY_ID, new DesktopP2PConnectionBuilders());
+const node = new InterconnectedNode(
+  MY_ID,
+  new DesktopP2PConnectionBuilders(),
+  DeviceType.DESKTOP
+);
 
 console.log('My ID: ' + MY_ID);
 node.start(BROKER_SERVICE_ADDRESS);
