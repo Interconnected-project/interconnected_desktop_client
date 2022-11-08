0c437f080ca28d45ee7a6bcdffbeb48e271ca91f
Alessandro Talmi <alessandro.talmi@studio.unibo.it>
2022-11-08 10:48:20 +0100

    changed BROKER_SERVICE_ADDRESS

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { v4 as uuidv4 } from 'uuid';
 import DesktopP2PConnectionBuilders from './desktop-specific_node_implementation/DesktopP2PConnectionBuilders';
 
 const BROKER_SERVICE_ADDRESS =
-  'http://ec2-3-208-18-248.compute-1.amazonaws.com:8000';
+  'http://ec2-3-73-1-24.eu-central-1.compute.amazonaws.com:8000';
 export const MY_ID = uuidv4();
 const node = new InterconnectedNode(
   MY_ID,
